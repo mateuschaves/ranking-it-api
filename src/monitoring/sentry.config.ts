78808e9724d3f291c0ceeb4c9df976f0a9fe1010
Mateus Henrique <mateushenriquechaves1@gmail.com>
2025-09-07 19:46:35 -0300

    feat: integrate Sentry for error tracking and monitoring
    - Added @sentry/nestjs dependency to the project.
    - Configured Sentry in the application with a dedicated monitoring setup.
    - Updated app.module.ts to include SentryModule for error handling.
    - Created a new sentry.config.ts file for Sentry initialization and configuration.

---
+++ src/monitoring/sentry.config.ts
@@ -0,0 +1,13 @@
+// Import with `const Sentry = require("@sentry/nestjs");` if you are using CJS
+import * as Sentry from "@sentry/nestjs"
+
+Sentry.init({
+  dsn: "https://7b821f95712d42ce159b826bbf9e63a6@o4509980686417920.ingest.us.sentry.io/4509980749660160",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  environment: process.env.NODE_ENV,
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+  enableLogs: true,
+});
