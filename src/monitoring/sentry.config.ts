cd61e0ca1a692917a82fdb4b46f4be26a713b97c
Mateus Henrique <mateushenriquechaves1@gmail.com>
2025-09-09 08:12:14 -0300

    feat: enhance Sentry configuration with profiling integration
    - Added nodeProfilingIntegration from @sentry/profiling-node to improve performance monitoring.
    - Configured profileSessionSampleRate and profileLifecycle options for better profiling control.

--- src/monitoring/sentry.config.ts
+++ src/monitoring/sentry.config.ts
@@ -1,5 +1,6 @@
 // Import with `const Sentry = require("@sentry/nestjs");` if you are using CJS
 import * as Sentry from "@sentry/nestjs"
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: "https://7b821f95712d42ce159b826bbf9e63a6@o4509980686417920.ingest.us.sentry.io/4509980749660160",
@@ -11,8 +12,14 @@ Sentry.init({
     Sentry.prismaIntegration(),
     Sentry.consoleLoggingIntegration(),
     Sentry.httpIntegration(),
+    nodeProfilingIntegration(),
+    
   ],
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
   enableLogs: true,
+  // Set sampling rate for profiling - this is evaluated only once per SDK.init call
+  profileSessionSampleRate: 1.0,
+  // Trace lifecycle automatically enables profiling during active traces
+  profileLifecycle: 'trace',
 });
